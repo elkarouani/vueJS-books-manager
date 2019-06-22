cd2c8913c85f4ef9ca0ee2320003e564781cffc7
KDragon <elkarouani@gmail.com>
2019-06-22 12:41:41 +0100

    Task: defining routes for added views

--- src/router.js
+++ src/router.js
@@ -20,6 +20,21 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/create',
+      name: 'Create',
+      component: () => import('./views/Create.vue')
+    },
+    {
+      path: '/edit',
+      name: 'Edit',
+      component: () => import('./views/Edit.vue')
+    },
+    {
+      path: '/show',
+      name: 'Show',
+      component: () => import('./views/Show.vue')
     }
   ]
 })
