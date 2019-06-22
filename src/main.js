5c6fef25c1a6d5dd63c5fd35864c3ddf5d4b644f
KDragon <elkarouani@gmail.com>
2019-06-22 15:51:08 +0100

    Task: setting up nprogress

--- src/main.js
+++ src/main.js
@@ -6,10 +6,23 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueRouter from 'vue-router'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'nprogress/nprogress.css'
+import NProgress from 'nprogress'
 
 Vue.config.productionTip = false
 App.use(VueRouter)
 App.use(VueAxios, axios)
+
+router.beforeResolve((to, from, next) => {
+  if (to.name) {
+    NProgress.start()
+  }
+  next()
+})
+
+router.afterEach(() => {
+  NProgress.done()
+})
 
 new Vue({
   router,
