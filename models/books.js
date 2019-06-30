8c5c58928761727e229836d5054b50af88fe3dfa
KDragon <elkarouani@gmail.com>
2019-06-30 13:41:12 +0100

    Task: adding books model

---
+++ models/books.js
@@ -0,0 +1,18 @@
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let Book = new Schema({
+    book_title: {
+        type: String
+    },
+    autor_name: {
+        type: String
+    },
+    price: {
+        type: Number
+    }
+},{
+    collection: 'books'
+});
+
+module.exports = mongoose.model('Book', Book);
